094eaa4fc0887a4e647eeccd49ee7cdc6ef2498b
0x4lucard <marcelofilho01@outlook.com>
2023-09-21 19:13:23 -0300

    Coding

---
+++ indo_ali/script.js
@@ -0,0 +1,12 @@
+const imagem = document.querySelector('.img');
+
+
+document.getElementById('meu-botao');
+document.imageContainer = document.getElementById(".img")
+document.addEventListener("click", () => {
+    audio.play();
+        button.style.zIndex = 0;
+        setTimeout(() => {
+            imageContainer.style.opacity = 0;
+        }, 100);
+    });
