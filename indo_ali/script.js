4036c0f1325f589b4b0382185f577bb5bd2acfa3
0x4lucard <marcelofilho01@outlook.com>
2023-11-29 00:42:28 -0300

    Update the code

--- indo_ali/script.js
+++ indo_ali/script.js
@@ -1,12 +1,11 @@
-const imagem = document.querySelector('.img');
-
-
-document.getElementById('meu-botao');
-document.imageContainer = document.getElementById(".img")
-document.addEventListener("click", () => {
-    audio.play();
+var button = document.getElementById('meu-botao');
+var imageContainer = document.getElementById("imageContainer");
+    button.addEventListener("click", () => {
+        audio.play();
         button.style.zIndex = 0;
         setTimeout(() => {
-            imageContainer.style.opacity = 0;
+            imageContainer.style.opacity = 1;
         }, 100);
     });
+
+
